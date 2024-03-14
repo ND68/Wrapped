11ef24d737895a350c23c134e5ae1c31088840f1
Nathan <nathandong68@gmail.com>
2024-03-14 14:50:20 -0400

    added java for xml pages

---
+++ Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/LandingPage.java
@@ -0,0 +1,6 @@
+package com.example.spotifywrappedgroup5;
+
+import androidx.fragment.app.Fragment;
+
+public class LandingPage extends Fragment {
+}
---
+++ Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/SignupPage.java
@@ -0,0 +1,6 @@
+package com.example.spotifywrappedgroup5;
+
+import androidx.fragment.app.Fragment;
+
+public class SignupPage extends Fragment {
+}
