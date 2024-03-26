c7394a388cb1759a49b6660918b9847e99f05a39
Nathan <nathandong68@gmail.com>
2024-03-26 15:24:33 -0400

    firebase authentication with email and password complete

--- Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/LandingPage.java
+++ Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/LandingPage.java
@@ -1,9 +1,12 @@
 package com.example.spotifywrappedgroup5;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,9 +15,16 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.spotifywrappedgroup5.databinding.LandingPageBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LandingPage extends Fragment {
     private LandingPageBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     public View onCreateView(
@@ -30,6 +40,40 @@ public class LandingPage extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
+        EditText loginEmail = view.findViewById(R.id.userNameInput);
+        EditText loginPass = view.findViewById(R.id.passwordInput);
+        Button loginButton = view.findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString().trim();
+                String pass = loginPass.getText().toString().trim();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(getActivity(), "Login Successful", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Login Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        loginPass.setError("Password cannot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter valid email");
+                }
+            }
+        });
         view.findViewById(R.id.singup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/SignupPage.java
+++ Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/SignupPage.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.spotifywrappedgroup5.databinding.SignupPageBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SignupPage extends Fragment {
     private SignupPageBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     public View onCreateView(
@@ -27,6 +37,39 @@ public class SignupPage extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        EditText signupEmail = view.findViewById(R.id.email_id);
+        EditText signupPass = view.findViewById(R.id.password);
+        Button signupButton = view.findViewById(R.id.submit_signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPass.getText().toString().trim();
+
+                if (user.isEmpty()) {
+                    signupEmail.setError("Email cannot be empty");
+                }
+
+                if (pass.isEmpty()) {
+                    signupPass.setError("Password cannot be empty");
+                } else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(), "Signup Successful", Toast.LENGTH_SHORT).show();
+                                NavHostFragment.findNavController(SignupPage.this)
+                                        .navigate(R.id.action_SignUpPage_to_LandingPage);
+                            } else {
+                                Toast.makeText(getActivity(), "Signup Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
     };
 
