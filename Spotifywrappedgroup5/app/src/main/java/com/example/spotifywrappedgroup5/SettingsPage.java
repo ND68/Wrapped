114dc14313bd46caf74de7fe7c1cce8786843192
Nathan <nathandong68@gmail.com>
2024-04-05 16:57:42 -0400

    settings pages, update info and delete account button

--- Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/SettingsPage.java
+++ Spotifywrappedgroup5/app/src/main/java/com/example/spotifywrappedgroup5/SettingsPage.java
@@ -2,23 +2,56 @@ package com.example.spotifywrappedgroup5;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.spotifywrappedgroup5.databinding.LandingPageBinding;
 import com.example.spotifywrappedgroup5.databinding.SettingsPageBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingsPage extends Fragment {
 
     private SettingsPageBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = SettingsPageBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("users");
+
+        Button update_info_button = view.findViewById(R.id.update_info);
+        Button delete_account_button = view.findViewById(R.id.delete_account);
+
+        update_info_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext())
+                        .setTitle("Update Information");
+            }
+        });
+
+        delete_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
